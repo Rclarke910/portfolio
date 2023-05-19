118b04a1166936a3522789df865e9d653df2b1b3
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2023-05-19 15:28:52 -0400

    added navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Home from './componet/Home'
 import Navbar from './componet/Navbar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
